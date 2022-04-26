bc50435c58c0482f6ad49af1dc7404e42f3e5208
Denis <64834314+sanddwich@users.noreply.github.com>
2022-04-26 18:48:57 +0400

    213

--- src/Layouts/MainLayout/Pages/Main/Block1/Block1.tsx
+++ src/Layouts/MainLayout/Pages/Main/Block1/Block1.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Row } from 'react-bootstrap'
+import { Col, Container, Row } from 'react-bootstrap'
 import ButtonComponent from '../../../../../SharedComponents/ButtonComponent/ButtonComponent'
 import './Block1.scss'
 
@@ -24,24 +24,36 @@ const Block1 = (props: Block1Props) => {
         backgroundSize: 'cover',
       }}
     >
-      <Container className="Block1__block p-0">
+      <Container className="Block1__block">
         <div className="Block1__Row1 d-flex justify-content-between align-items-baseline">
           <div className="Block1__Logo">
             <div className="Block1__LogoText">New Generation</div>
             <div className="Block1__LogoLine"></div>
           </div>
-          <div className="Block1__navigation d-flex flex-wrap justify-content-center">
+          <div className="Block1__navigation d-none d-lg-flex flex-wrap justify-content-center">
             <div className="Block1__navigationEl">Типы систем</div>
             <div className="Block1__navigationEl">Этапы</div>
             <div className="Block1__navigationEl">Выполненые проекты</div>
             <div className="Block1__navigationEl">Отзывы</div>
             <div className="Block1__navigationEl">Контакты</div>
           </div>
-          <div className="Block1__callButton">
+          <div className="Block1__callButton d-none d-sm-block">
             <ButtonComponent onClick={callOrder}>
               <div>Заказать звонок</div>
             </ButtonComponent>
           </div>
+        </div>
+
+        <Row className='Block1__titleRow d-flex justify-content-center'>
+          <Col xs={12} xxl={9} className='Block1__titleCol'>
+            <div className='Block1__title'>
+              Солнечные электростанции для дома и бизнеса
+            </div>
+          </Col>
+        </Row>
+
+        <div className='Block1__subtitle'>
+          Шагни в будущее вместе с нами
         </div>
       </Container>
 
